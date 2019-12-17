a53b74b942b36ad42a31f35a27469e42e765111a
kheirlb <kheirlbparks@gmail.com>
2019-12-17 01:03:18 -0800

    new website

---
+++ js/main.js
@@ -0,0 +1,18 @@
+const typeID = document.getElementById("typeSpot");
+
+var waitTime = 6000;
+
+var typed = new Typed(typeID, {
+  strings: ["",
+            "^500Hello.",
+            "My name is Karl Parks.",
+            "Welcome to my website.^1500 :)^3000",
+            "See more below^1000.^1000.^1000.^1000 enjoy!",
+            ],
+  typeSpeed: 40,
+  backSpeed: 25,
+});
+
+setTimeout(function(){
+    $('.jumbo2').css('background-image', 'url("/img/7K_karlLeaning2.jpeg")');
+}, waitTime)
