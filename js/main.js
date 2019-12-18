fb1369225579c4802147ca1ebde089efe9290825
kheirlb <kheirlbparks@gmail.com>
2019-12-17 19:26:35 -0800

    small edits

--- js/main.js
+++ js/main.js
@@ -1,21 +1,27 @@
 const typeID = document.getElementById("typeSpot");
 
+// "^500Hello.",
+// "My name is Karl Parks.",
+// "Welcome to my website.^1500 :)",
+
+// "^500Hello.^1000 <br>>^500 My name is Karl Parks.^1000 <br>>^500 Welcome to my website.^1500 :)",
+
 //typer
 var typed = new Typed(typeID, {
   strings: ["",
-            "^500Hello.",
-            "My name is Karl Parks.",
-            "Welcome to my website.^1500 :)",
+  "^500Hello.",
+  "My name is Karl Parks.",
+  "Welcome to my website.^1500 :)",
             ],
   typeSpeed: 40,
   backSpeed: 25,
 });
 
-//background delay
-var waitTime = 6000;
-setTimeout(function(){
-    $('.jumbo2').css('background-image', 'url("/img/7K_karlLeaning2.jpeg")');
-}, waitTime)
+// //background delay
+// var waitTime = 6000;
+// setTimeout(function(){
+//     $('.jumbo2').css('background-image', 'url("/img/7K_karlLeaning2.jpeg")');
+// }, waitTime)
 
 //plotly
 var skillVar = document.getElementById('skillDiv');
@@ -37,7 +43,7 @@ ikLink = "<a style='color: blue' href='https://github.com/Kheirlb/CS556/blob/mas
 var robotics = ["Jacobians", "Genetic<br>Algorithms", "PID<br>Controllers", ikLink];
 var robotParents = Array(robotics.length).fill("Robotics");
 
-var leadership = ["<a style='color: blue' href='https://www.sdsurocketproject.org/our-team'>President</a>", "Senior<br>Engineer<br>(SDSURP)", "Design Lead<br>(SDSURP)", "CESC<br>VP of<br>Programming","Resident<br>Advisor","RHA Pres"];
+var leadership = ["<a style='color: blue' href='https://www.sdsurocketproject.org/our-team'>President<br>SDSURP</a>", "Senior<br>Engineer<br>SDSURP", "Design Lead<br>SDSURP", "CESC<br>VP of<br>Programming","Resident<br>Advisor","RHA Pres"];
 var leadershipParents = Array(leadership.length).fill("Leadership");
 
 var electrical = ["Breadboards", "PCBs", "Soldering", "Multimeters", "Oscilloscopes", "Microohm Meters"];
