d2f1d42788b9eda41f95c06257d794cef1e1c54a
kheirlb <kheirlbparks@gmail.com>
2020-01-09 19:35:44 -0800

    LE and HPB

--- js/main.js
+++ js/main.js
@@ -24,11 +24,11 @@ var typed = new Typed(typeID, {
   backSpeed: 25,
 });
 
-//background delay
-var waitTime = 1000;
-setTimeout(function(){
-    $('.jumbo2').css('background-image', 'url("/img/7K_karlLeaning2.jpeg")');
-}, waitTime)
+// //background delay
+// var waitTime = 1000;
+// setTimeout(function(){
+//     $('.jumbo2').css('background-image', 'url("/img/7K_karlLeaning2.jpeg")');
+// }, waitTime)
 
 //plotly
 var skillVar = document.getElementById('skillDiv');
@@ -40,8 +40,8 @@ pythonLink = "<a style='color: blue' href='https://github.com/Kheirlb/puretherma
 var languages = ["MATLAB", "Java", pythonLink, "C/C++", "LabView", "FORTRAN", "JavaScript", "Racket/Scheme", "Linux/Unix","Version Control"];
 var langParents = Array(languages.length).fill("Software");
 
-var cadPrograms = ["PTC Creo", "Solidworks", "Autodesk<br>Inventor", "Blender"];
-var cadParents = Array(cadPrograms.length).fill("CAD");
+var cadPrograms = ["PTC Creo/Simulate", "Solidworks", "FEMAP/NASTRAN", "Autodesk<br>Inventor", "Blender"];
+var cadParents = Array(cadPrograms.length).fill("CAD/FEA");
 
 var aerospaceBackground = ["Avionics", "Composites", "Cryogenics", "High Pressure", "Propulsion", "FEA", "PDM/PLM"];
 var aerospaceParents = Array(aerospaceBackground.length).fill("Aerospace");
@@ -50,13 +50,13 @@ ikLink = "<a style='color: blue' href='https://github.com/Kheirlb/CS556/blob/mas
 var robotics = ["Jacobians", "Genetic<br>Algorithms", "PID<br>Controllers", ikLink];
 var robotParents = Array(robotics.length).fill("Robotics");
 
-var leadership = ["<a style='color: blue' href='https://www.sdsurocketproject.org/our-team'>President<br>SDSURP</a>", "Senior<br>Engineer<br>SDSURP", "Design Lead<br>SDSURP", "CESC<br>VP of<br>Programming","Resident<br>Advisor","RHA Pres"];
+var leadership = ["<a style='color: blue' href='https://www.sdsurocketproject.org/our-team'>President<br>SDSURP</a>", "Senior<br>Engineer<br>SDSURP", "Design Lead<br>SDSURP", "VP of<br>Programming<br>CESC","Resident<br>Advisor","President<br>RHA"];
 var leadershipParents = Array(leadership.length).fill("Leadership");
 
 var electrical = ["Breadboards", "PCBs", "Soldering", "Multimeters", "Oscilloscopes", "Microohm Meters"];
 var electricalParents = Array(electrical.length).fill("Electrical");
 
-var mainCategories = [center, "Software", "CAD", "Aerospace", "Robotics", "Leadership", "Electrical"];
+var mainCategories = [center, "Software", "CAD/FEA", "Aerospace", "Robotics", "Leadership", "Electrical"];
 var combinedLabels = mainCategories.concat(languages, cadPrograms, aerospaceBackground, robotics, leadership, electrical);
 var combinedParents = mainParents.concat(langParents, cadParents, aerospaceParents, robotParents, leadershipParents, electricalParents);
 
